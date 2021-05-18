8901f80e9bec2928fe5817df4fd53234cdb14499
KassioVenicios <kassio_venicios@hotmail.com>
2021-05-17 22:33:31 -0300

    Box-sizing: border-box para todos os elementos

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -5,7 +5,6 @@ export const AppContainer = styled.div`
   display: flex;
   height: 100vh;
   padding: 32px 40px;
-  box-sizing: border-box;
   background: url(${backImage}), linear-gradient(107.65deg, #1042DD 0.43%, #24B0A8 100%);
 `;
 
