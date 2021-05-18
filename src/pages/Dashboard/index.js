980b6a76bef5be0546cce98411c9fef15715471c
KassioVenicios <kassio_venicios@hotmail.com>
2021-05-18 12:46:27 -0300

    Botão "ver todos" em histórico de atendimentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,7 @@ import {
   CardsList,
   Section,
   SectionTitle,
+  SectionTitleButton,
 } from './styles';
 
 function Dashboard() {
@@ -11,14 +12,22 @@ function Dashboard() {
   const Info = useSelector(state => state.info);
 
   return (
-    <Section>
-      <SectionTitle>Informações Gerais</SectionTitle>
-      <CardsList>
-        {
-          Info.map(item => <InfoCard key={item.id} item={item} />)
-        }
-      </CardsList>
-    </Section>
+    <>
+      <Section>
+        <SectionTitle>Informações Gerais</SectionTitle>
+        <CardsList>
+          {
+            Info.map(item => <InfoCard key={item.id} item={item} />)
+          }
+        </CardsList>
+      </Section>
+      <Section>
+        <SectionTitle>
+          Histórico de atendimentos
+          <SectionTitleButton>ver todos</SectionTitleButton>
+        </SectionTitle>
+      </Section>
+    </>
   );
 }
 
